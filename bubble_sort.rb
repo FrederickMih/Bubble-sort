963bdd544f33fbea84af5c50dae50154c8e01c73
FrederickMih <fredtimermih@gmail.com>
2021-01-20 03:47:57 +0100

    bubble sorting of numbers completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,21 @@
-puts "sorting"
+def bubble_sort(array)
+  n = array.length
+  loop do
+    # [1, 3, 2, 1, 8, 4, 3, 3]
+    swapped_test = false
+    (n - 1).times do |j|
+      next unless array[j] > array[j + 1]
+
+      # swap
+      temp = array[j]
+      array[j] = array[j + 1]
+      array[j + 1] = temp
+
+      swapped_test = true
+    end
+    break unless swapped_test
+  end
+  array
+end
+a = [1, 3, 2, 1, 8, 4, 3, 3]
+p bubble_sort(a)
